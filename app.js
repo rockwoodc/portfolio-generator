38423cccf45f215bad89fe570b3e6aedb1719bc9
rockwoodc <rockwoodce@gmail.com>
2022-07-16 15:15:10 -0400

    generate html and modularize code

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
+//required to use the fs module
+const fs = require('fs');
+
+//With this statement, the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js file. Note here that the variable name is arbitrary; however, the relative path to include the file must be exact.
+const generatePage = require('./src/page-template.js');
+
+//process=represents what is going on in the app, argv= what holds what is typed in the command line, slice= returns and array
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    //this...
-    for (let i = 0; i < profileDataArr.length; i += 1){
-    console.log(profileDataArr[i]);
-    }
+//extract those arguments and store them into distinct variables
+//es6 feautre that destructures
+const [name, github] = profileDataArgs;
+//Below will work the same as the const above
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-    console.log('============');
 
-    //is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//The first argument is the file name that will be created, or the output file. The second argument is the data that's being written: the HTML string template. The third argument is the callback function that will handle any errors as well as the success message.
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-};
+    console.log('Portfolio complete! Check out index.html to see the output!')
+})
 
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     //this...
+//     for (let i = 0; i < profileDataArr.length; i += 1){
+//     console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     //is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// };
+
+// printProfileData(profileDataArgs);
---
+++ src/page-template.js
@@ -0,0 +1,23 @@
+//the backticks make these temperate literals. user input will repalce what is in thecurly brackets
+const generatePage = (name, github) => {
+    //function that receives the command-line arguments and inserts them in a HTML template literal
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+
+};
+
+module.exports = generatePage;
