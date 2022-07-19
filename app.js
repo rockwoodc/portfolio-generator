40481ca37d23aec73c59727b1ad8b9d5270d5171
rockwoodc <rockwoodce@gmail.com>
2022-07-19 02:47:15 -0400

    created gitignore and added first question

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 //required to use the fs module
-const fs = require('fs');
+// const fs = require('fs');
 
 //With this statement, the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js file. Note here that the variable name is arbitrary; however, the relative path to include the file must be exact.
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
 
-//process=represents what is going on in the app, argv= what holds what is typed in the command line, slice= returns and array
-const profileDataArgs = process.argv.slice(2);
-
-//extract those arguments and store them into distinct variables
-//es6 feautre that destructures
-const [name, github] = profileDataArgs;
-//Below will work the same as the const above
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+// const pageHMTL = generatePage(name, github);
 
 
 //The first argument is the file name that will be created, or the output file. The second argument is the data that's being written: the HTML string template. The third argument is the callback function that will handle any errors as well as the success message.
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHMTL, err => {
+//     if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
+
+
+
 
 // const printProfileData = profileDataArr => {
 //     //this...
@@ -36,3 +42,13 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 // };
 
 // printProfileData(profileDataArgs);
+
+//process=represents what is going on in the app, argv= what holds what is typed in the command line, slice= returns and array
+// const profileDataArgs = process.argv.slice(2);
+
+//extract those arguments and store them into distinct variables
+//es6 feautre that destructures
+// const [name, github] = profileDataArgs;
+//Below will work the same as the const above
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
