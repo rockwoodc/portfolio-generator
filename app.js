aa54673c2c2d2b6d60d69533c65c68536ddc35f8
rockwoodc <rockwoodce@gmail.com>
2022-07-19 18:50:20 -0400

    refactored html and made arrays for projects

--- app.js
+++ app.js
@@ -133,44 +133,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  })
+    const pageHTML = generatePage(portfolioData);
 
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-// const pageHMTL = generatePage(name, github);
-
-
-//The first argument is the file name that will be created, or the output file. The second argument is the data that's being written: the HTML string template. The third argument is the callback function that will handle any errors as well as the success message.
-// fs.writeFile('./index.html', pageHMTL, err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// })
-
-
-
-
-// const printProfileData = profileDataArr => {
-//     //this...
-//     for (let i = 0; i < profileDataArr.length; i += 1){
-//     console.log(profileDataArr[i]);
-//     }
-
-//     console.log('================');
-
-//     //is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-
-// };
-
-// printProfileData(profileDataArgs);
-
-//process=represents what is going on in the app, argv= what holds what is typed in the command line, slice= returns and array
-// const profileDataArgs = process.argv.slice(2);
-
-//extract those arguments and store them into distinct variables
-//es6 feautre that destructures
-// const [name, github] = profileDataArgs;
-//Below will work the same as the const above
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
